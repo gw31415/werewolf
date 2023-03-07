ce62fae3bad82f83b0238fa494ea3028fdb65fd4
gw31415 <git@amas.dev>
2023-03-07 16:36:53 +0900

    add Message trait & State struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+mod werewolf {
+    use serde::{Deserialize, Serialize};
+    use std::ops::AddAssign;
+
+    /// 送受信されるメッセージ
+    pub trait Message: Serialize + for<'a> Deserialize<'a> {
+        /// メッセージの種類を識別するID
+        const TYPEID: u64;
+        /// Stateを更新する。
+        fn update(&self, state: &mut State);
+    }
+
+    /// ゲームの状態
+    #[derive(Serialize, Deserialize)]
+    pub struct State;
+
+    impl<T> AddAssign<T> for State
+    where
+        T: Message,
+    {
+        fn add_assign(&mut self, rhs: T) {
+            rhs.update(self);
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }

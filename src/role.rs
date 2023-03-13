7d857389fbec282881c8479b1a1c9c22958da008
gw31415 <git@amas.dev>
2023-03-14 00:24:43 +0900

    add Role::Citizen and method team, add Debug derive to Team

--- src/role.rs
+++ src/role.rs
@@ -2,10 +2,22 @@ use serde::{Deserialize, Serialize};
 
 /// 役職
 #[derive(Serialize, Deserialize, PartialEq, Clone)]
-pub enum Role {}
+pub enum Role {
+    /// 市民
+    Citizen,
+}
+
+impl Role {
+    /// チームを返す。
+    pub fn team(&self) -> Team {
+        match self {
+            Self::Citizen => Team::Citizen,
+        }
+    }
+}
 
 /// 陣営
-#[derive(Serialize, Deserialize, PartialEq, Clone)]
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub enum Team {
     /// 市民
     Citizen,

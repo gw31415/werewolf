04872d770a75d94d3678374a6e8c9b372a11b99c
gw31415 <git@amas.dev>
2023-03-14 00:37:45 +0900

    add Vote request

--- src/request.rs
+++ src/request.rs
@@ -1,12 +1,82 @@
-use super::{Error, Name, State};
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+use crate::{Error, Name, Phase, State};
 
 /// 送受信されるリクエスト
 #[derive(Serialize, Deserialize)]
-pub enum Request {}
-impl Request {
+pub enum Request {
+    /// 投票: 生存者・日中
+    Vote(Name),
+}
+impl<'state> Request {
     /// Stateを更新する。
-    pub fn apply_to(&self, state: &mut State, sender: &Name) -> Result<(), Error> {
-        todo!()
+    pub fn apply_to(&self, state: &'state mut State, sender: &Name) -> Result<(), Error> {
+        /// フェーズの確認をする
+        macro_rules! assert_phase {
+            ($expected:pat) => {
+                let $expected = state.phase else {
+                    return Err(Error::InvalidPhase {
+                        found: state.phase.to_owned(), expected: stringify!($expected).to_string(),
+                    });
+                };
+            };
+        }
+        /// 生存者に限るリクエストである
+        macro_rules! survivors_only {
+            () => {
+                if !state.survivors.contains(sender) {
+                    return Err(Error::SurvivorsOnly);
+                }
+            };
+        }
+
+        match self {
+            Self::Vote(vote_to) => {
+                // 日中に限る
+                assert_phase!(Phase::Day { ref mut votes, ref mut candidates });
+                // 生存者に限る
+                survivors_only!();
+
+                if !candidates.contains(vote_to) {
+                    // 指定された名前が候補者に含まれていない場合。
+                    return Err(Error::CannotVoteToThisPlayer(vote_to.to_owned()));
+                }
+                // 投票リストの更新
+                votes.insert(sender.to_owned(), vote_to.to_owned());
+                if votes.len() == state.survivors.len() {
+                    // 全員投票が終わったら
+                    // 投票の集計
+                    let mut max = 0; // 最大の得票数
+                    candidates.clear(); // 最大得票の候補者を洗いだす
+                    let mut votes_count = HashMap::new(); // 得票数
+                    for candidate in votes.values() {
+                        let counter = votes_count.entry(candidate).or_insert(0);
+                        *counter += 1;
+                        if max <= *counter {
+                            if max < *counter {
+                                max = *counter;
+                                candidates.clear();
+                            }
+                            candidates.insert(candidate.to_owned());
+                        }
+                    }
+                    if candidates.len() != 1 {
+                        // 追放者が決まらなかった場合
+                        // 決戦投票
+                        votes.clear();
+                        return Ok(());
+                    }
+                    let exiled_player = candidates.iter().next().unwrap(); // 追放される人
+                    state.survivors.remove(exiled_player);
+                    // 勝敗確認
+                    if !state.judge() {
+                        // 勝敗が決まらなかった場合
+                        state.phase = Phase::Night;
+                    }
+                };
+                Ok(())
+            }
+        }
     }
 }

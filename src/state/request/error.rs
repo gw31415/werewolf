aef37e08a2c2ee7089cc5f18a9f88d00d112d987
gw31415 <git@amas.dev>
2023-03-16 16:59:16 +0900

    split request module

---
+++ src/state/request/error.rs
@@ -0,0 +1,19 @@
+use super::Phase;
+use thiserror::Error;
+
+/// リクエスト処理時のエラー
+#[derive(Error, Debug)]
+pub enum Error {
+    /// 場面が適切でない場合
+    #[error("invalid Phase (found {found:?}, expected pattern {expected:?})")]
+    InvalidPhase { found: Box<Phase>, expected: String },
+    /// ターゲットが適切でない場合
+    #[error("you cannot request about `{0}`.")]
+    InvalidTarget(String),
+    /// リクエスト元が追放または殺害されている場合
+    #[error("this request is allowed only survivors.")]
+    SurvivorsOnly,
+    /// 場面あたりのリクエスト回数を超過した場合
+    #[error("cannot act more than once")]
+    MultipleActions,
+}
